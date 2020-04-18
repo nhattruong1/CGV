db270698e5cd90df12c4299ac2862cbd07968bfe
nhattruong1 <nhattruong12c2@gmail.com>
2020-04-18 16:22:19 +0700

    make it better

--- db/connect.php
+++ db/connect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost","root","","ticketbooking");
+$mysqli = new mysqli("i2cpbxbi4neiupid.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","fwn76oocoovtotwl","owx541k4ia15n5f8","txxvtu25s5m7n55h");
 
 // Check connection
 if ($mysqli -> connect_errno) {
