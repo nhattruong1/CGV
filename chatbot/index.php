718f25136621bb07e733cc276ad4b7d775707b0c
nhattruong1 <nhattruong12c2@gmail.com>
2020-05-21 00:18:48 +0700

    chatbot

--- chatbot/index.php
+++ chatbot/index.php
@@ -1,3 +1,43 @@
 <?php
-echo "hi";
+
+/* validate verify token needed for setting up web hook */
+if (isset($_GET['hub_verify_token'])) {
+    if ($_GET['hub_verify_token'] === '123') {
+        echo $_GET['hub_challenge'];
+        return;
+    } else {
+        echo 'Invalid Verify Token';
+        return;
+    }
+}
+
+/* receive and send messages */
+$input = json_decode(file_get_contents('php://input'), true);
+if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
+
+    $sender = $input['entry'][0]['messaging'][0]['sender']['id']; //sender facebook id
+    $message = $input['entry'][0]['messaging'][0]['message']['text']; //text that user sent
+
+    $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=PAGE_ACCESS_TOKEN';
+
+    /*initialize curl*/
+    $ch = curl_init($url);
+    /*prepare response*/
+    $jsonData = '{
+    "recipient":{
+        "id":"' . $sender . '"
+        },
+        "message":{
+            "text":"You said, ' . $message . '"
+        }
+    }';
+    /* curl setting to send a json post data */
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    if (!empty($message)) {
+        $result = curl_exec($ch); // user will get the message
+    }
+}
+
 ?>
